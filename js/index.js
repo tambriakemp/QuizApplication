da78c1f0400ab901697661cae6cceb349e4ea119
[tambriakemp] <info@cre8visions.com>
2018-09-18 19:09:00 -0400

    initial

---
+++ js/index.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const QUESTIONS = [
+    {question: "Inside which HTML element do we put the JavaScript?", 
+    a: "<js>" , 
+    b: "<javascript>", 
+    c: "<script>",
+    d: "<scripting>"
+},
+    {question: "What is the correct syntax for referring to an external script called 'xxx.js'?", 
+    a: "<script href='xxx.js'>", 
+    b: "<script name='xxx.js'>", 
+    c: "<script src='xxx.js'>",
+    d: "<script ='xxx.js'>"
+},    
+    {question: "How do you write 'Hello World' in an alert box?", 
+    a: "msgBox(‘Hello World’);", 
+    b: "alert(‘Hello World’);", 
+    c: "msg(‘Hello World’); ",
+    d: "alertBox(‘Hello World’);"
+
+},
+    {question: "How do you create a function in JavaScript?", 
+    a: "function = myFunction()", 
+    b: "function:myFunction()", 
+    c: "function myFunction()",
+    d: " myFunction()"
+},
+    {question: "How do you call a function named 'myFunction'?", 
+    a: "call myFunction()", 
+    b: "call function myFunction()", 
+    c: "myFunction()",
+    d: "myFunction.call()"
+},
+    {question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?", 
+    a: "if (i <> 5)", 
+    b: "if i =! 5 then", 
+    c: "if i <> 5",
+    d: "if (i != 5)"
+},
+    {question: "How to write an IF statement in JavaScript?", 
+    a: "if i = 5", 
+    b: "if i == 5 then", 
+    c: "if (i == 5)",
+    d: "if i = 5 then"
+},
+    {question: "How does a WHILE loop start?", 
+    a: "while (i <= 10; i++)", 
+    b: "while i = 1 to 10", 
+    c: "while (i <= 10)",
+    d: "while (i)"
+},
+    {question: "How does a FOR loop start?", 
+    a: "for i = 1 to 5", 
+    b: "for (i = 0; i <= 5)", 
+    c: "for (i = 0; i <= 5; i++)",
+    d: "for (i <= 5; i++)"
+},
+    {question: "How can you add a comment in a JavaScript?", 
+    a: "<!--This is a comment-->", 
+    b: "//This is a comment", 
+    c: "'This is a comment",
+    d: "!--This is a comment"
+}
+
+
+]
